854191db2d3d55d19c161ee982d705b32f0fa8fc
Henrique Silva <henriqesilva@asus.home>
2013-06-02 20:06:42 +0100

    Comentarios Adicionados

--- js/script.js
+++ js/script.js
@@ -5,22 +5,24 @@ var arrayReleituras=[
 {id:"sessao2",titulo:"2",descricao:"O Tempo: eu tenho que inventar coisas para escrever aqui",img:"img/sessao2.png"},
 {id:"sessao1",titulo:"1",descricao:"Aquela sessão que não correu lá muito bem",img:"img/sessao1.png"}]
 
+
+
+//Define a largura da página dependendo do numero de elementos
 $("body").css({width:(arrayReleituras.length*300)+"px"});
 $("html").css({width:(arrayReleituras.length*300)+"px"});
 
 
+//Cria as divs rect e o conteúdo das mesmas
 for (var i = 0; i < arrayReleituras.length; i++) {
 	$("#float").append('<div class="rect" id=\"'+arrayReleituras[i].id+'\">'+'<h3 class="titulo">'+arrayReleituras[i].titulo+'</h3>'+'<p class="descricao">'+arrayReleituras[i].descricao+'</p>'+'</div>');
-
 	 $('#'+arrayReleituras[i].id).css({"background-image":"url('"+arrayReleituras[i].img+"')"});
-
 };
 
-$(".rect").on("mouseover",funcao);
+$(".rect").on("mouseover",transp);
 
 
-
-function funcao(data){
+//funcao responsavel por mudar a opacidade das divs rect
+function transp(data){
 
 	//Torna todas as divs rect um pouco transparentes
 	$(".rect").css({"opacity":0.5,"box-shadow":""});

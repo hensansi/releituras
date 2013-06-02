67bc4166689ae3a7255a553c79f4d49139a3718e
Henrique Silva <henriqesilva@asus.home>
2013-06-02 18:09:15 +0100

    Largura do html e body dinamicos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var arrayReleituras=[{id:"sessao5",titulo:"Sessão 5",descricao:"A quinta releitura levou-nos à Tipografia Camões",img:"img/sessao5.png"},{id:"sessao4",titulo:"Sessão 4",descricao:"A revolução de Abril",img:"img/sessao4.png"},{id:"sessao3",titulo:"Sessão 3",descricao:"O soneto e o som",img:"img/sessao3.png"}]
 
+$("body").css({width:(arrayReleituras.length*300)+"px"});
+$("html").css({width:(arrayReleituras.length*300)+"px"});
+
+
 for (var i = 0; i < arrayReleituras.length; i++) {
 	$("#float").append('<div class="rect" id=\"'+arrayReleituras[i].id+'\">'+'<h3 class="titulo">'+arrayReleituras[i].titulo+'</h3>'+'<p class="descricao">'+arrayReleituras[i].descricao+'</p>'+'</div>');
 

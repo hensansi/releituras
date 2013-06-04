b722bfc643beac66e4461b3781f9b0517a800b47
Henrique Silva <henriqesilva@asus.home>
2013-06-04 15:44:02 +0100

    Clicks de submenus introduzidos

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ var arrayReleituras=[
 {id:"sessao2",titulo:"2",descricao:"O Tempo: eu tenho que inventar coisas para escrever aqui",img:"img/sessao2.png"},
 {id:"sessao1",titulo:"1",descricao:"Aquela sessão que não correu lá muito bem",img:"img/sessao1.png"}]
 
+var arrayReleitura=[
+{id:"releitura2",titulo:"Miguel Carepa, Ernesto Cruz & Bruno Santos",descricao:"O código morse",img:"img/sessao2.png"},
+{id:"releitura1",titulo:"Henrique Silva & Luis Ribeiro",descricao:"Urra Mesmo Ora",img:"img/sessao1.png"}]
+
 
 
 //Define a largura da página dependendo do numero de elementos
@@ -15,19 +19,49 @@ $("html").css({width:(arrayReleituras.length*300)+"px"});
 //Cria as divs rect e o conteúdo das mesmas
 for (var i = 0; i < arrayReleituras.length; i++) {
 	$("#float").append('<div class="rect" id=\"'+arrayReleituras[i].id+'\">'+'<h3 class="titulo">'+arrayReleituras[i].titulo+'</h3>'+'<p class="descricao">'+arrayReleituras[i].descricao+'</p>'+'</div>');
-	 $('#'+arrayReleituras[i].id).css({"background-image":"url('"+arrayReleituras[i].img+"')"});
+	$('#'+arrayReleituras[i].id).css({"background-image":"url('"+arrayReleituras[i].img+"')"});
 };
 
 $(".rect").on("mouseover",transp);
+$(".rect").on("click",subSessao);
+
 
 
 //funcao responsavel por mudar a opacidade das divs rect
 function transp(data){
 
+	var id="#"+data.currentTarget.id;
+
 	//Torna todas as divs rect um pouco transparentes
 	$(".rect").css({"opacity":0.5,"box-shadow":""});
 
+
 	//Torna a div sobre a qual o rato está sobre totalmente opaca
-	$("#"+data.currentTarget.id).css({"opacity":1,"box-shadow":"inset 0 0 10px #000"});
+	$(id).css({"opacity":1,"box-shadow":"inset 0 0 10px #000"});
+}
+
+function subSessao(data){
+	console.log("yeyy");
+	$("#float").html("");
+
+	//Redefine a largura
+	$("body").css({width:(arrayReleitura.length*300)+"px"});
+	$("html").css({width:(arrayReleitura.length*300)+"px"});
+
+	for (var i = 0; i < arrayReleitura.length; i++) {
+	$("#float").append('<div class="rect" id=\"'+arrayReleitura[i].id+'\">'+'<h3 class="artistas">'+arrayReleitura[i].titulo+'</h3>'+'<p class="descricao">'+arrayReleitura[i].descricao+'</p>'+'</div>');
+	$('#'+arrayReleitura[i].id).css({"background-image":"url('"+arrayReleitura[i].img+"')"});
+	};
+	$(".rect").on("mouseover",transp);
+	$(".rect").on("click",leitura);
+}
+
+function leitura(data){
+
+	console.log(data);
+	$("#float").html("");
+
 
 }
+
+
